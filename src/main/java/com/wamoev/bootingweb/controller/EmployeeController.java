2adfbb2d325d99b2327ea876380099945722cb4f
Shamoiev <wamoev@gmail.com>
2021-12-07 13:26:30 +0200

    Add Controllers

---
+++ src/main/java/com/wamoev/bootingweb/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.wamoev.bootingweb.controller;
+
+import com.wamoev.bootingweb.servises.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public String getEmployees(Model model) {
+        model.addAttribute("employees", employeeService.getEmployees());
+        return "employees";
+    }
+}
